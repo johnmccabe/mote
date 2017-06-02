2a1d026c259e64924cdae5383663d0f1588da769
John McCabe <john@johnmccabe.net>
2017-06-02 22:15:29 +0100

    Pass RGB values via args to rgb.go example

--- example/rgb/rgb.go
+++ example/rgb/rgb.go
@@ -2,10 +2,16 @@
 A simple static RGB colour setting example.
 
 This is a port of https://github.com/pimoroni/mote/blob/master/python/examples/rgb.py
+
+You can set the colour of your Mote sticks by passing the RGB values as arguments.
+
+  go run rgb.go 255 255 255
 */
 package main
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/johnmccabe/mote"
@@ -18,9 +24,13 @@ func main() {
 	mote.ConfigureChannel(3, 16, false)
 	mote.ConfigureChannel(4, 16, false)
 
+	r, _ := strconv.Atoi(os.Args[1])
+	g, _ := strconv.Atoi(os.Args[2])
+	b, _ := strconv.Atoi(os.Args[3])
+
 	for channel := 1; channel < 5; channel++ {
 		for pixel := 0; pixel < 16; pixel++ {
-			mote.SetPixel(channel, pixel, 0, 0, 0)
+			mote.SetPixel(channel, pixel, r, g, b)
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
